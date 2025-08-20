8ac355ba158b6ba53a5cb337a25cd81b6dcdae4b
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2025-08-20 10:16:06 +0200

    Create second getIngredients to test correct quantity handling

--- tests/item_test.ts
+++ tests/item_test.ts
@@ -77,3 +77,83 @@ Deno.test(async function getIngredientsTest() {
     assertEquals(actualUsage.quantity, expectedUsage.quantity);
   }
 });
+
+Deno.test(async function getIngredientsTest2() {
+  const item = await getItem("0021031111");
+
+  const expectedIngredients = [
+    // Dark Marci
+    {
+      ingredient: {
+        itemCode: "0000005100",
+        itemName: "Mørk chokolade Callebaut (CHD-P60ZV-105)",
+      },
+      quantity: 9 * 1 * 0.004197,
+    },
+    {
+      ingredient: {
+        itemCode: "0000005500",
+        itemName: "Marcipan valencia 60% uden kons (560015)",
+      },
+      quantity: 9 * 1 * 0.006303,
+    },
+
+    // Crispy Carrie
+    {
+      ingredient: {
+        itemCode: "0000005107",
+        itemName: "Mælkechokolade CHM-Q23-105",
+      },
+      quantity: 9 * 1 * 0.009005,
+    },
+    {
+      ingredient: {
+        itemCode: "0000006123",
+        itemName: "Soyacrisps (Supro nuggets)",
+      },
+      quantity: 9 * 1 * 0.000401,
+    },
+    {
+      ingredient: {
+        itemCode: "0000006088",
+        itemName: "Læsø Havsalt (13902)",
+      },
+      quantity: 9 * 1 * 0.00008,
+    },
+    {
+      ingredient: {
+        itemCode: "0000006141",
+        itemName: "Karamel flakes 1-5 mm (590207)",
+      },
+      quantity: 9 * 1 * 0.000901,
+    },
+  ];
+
+  const actualIngredients = await getIngredients(item);
+
+  assertEquals(
+    actualIngredients.length,
+    expectedIngredients.length,
+    "Ingredient count mismatch"
+  );
+
+  const expectedMap = new Map(
+    expectedIngredients.map((usage) => [usage.ingredient.itemCode, usage])
+  );
+
+  for (const actualUsage of actualIngredients) {
+    const expectedUsage = expectedMap.get(actualUsage.ingredient.itemCode);
+
+    if (!expectedUsage) {
+      throw new Error(
+        `Unexpected ingredient: ${actualUsage.ingredient.itemName}`
+      );
+    }
+
+    assertEquals(
+      actualUsage.ingredient.itemName,
+      expectedUsage.ingredient.itemName
+    );
+    assertEquals(actualUsage.quantity, expectedUsage.quantity);
+  }
+});
