60c70c07f0099d422cf20e5f154049449d202898
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2025-08-24 14:26:03 +0200

    Update integration test setup

--- tests/integration/sap-api_test.ts
+++ tests/integration/sap-api_test.ts
@@ -1,12 +1,20 @@
 import { assertEquals, assertExists } from "@std/assert";
 import { ItemService } from "../../src/sap/services/item-service.ts";
+import { setupTestData, teardownTestData } from "../fixtures/setup.ts";
+import { testItems } from "../fixtures/test_data.ts";
 
 Deno.test(async function getItemTest() {
-  const itemCode = "0021050008";
-  const item = await ItemService.getItem(itemCode);
+  await setupTestData();
 
-  assertExists(item);
-  assertEquals(item.itemCode, itemCode);
+  try {
+    const expected = testItems["test_0000001111_test"];
+    const actual = await ItemService.getItem(expected.itemCode);
+
+    assertExists(actual);
+    assertEquals(actual, expected);
+  } finally {
+    await teardownTestData();
+  }
 });
 
 Deno.test(async function getIngredientsTest() {
