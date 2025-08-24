2937d2545dceadf783cdfb4834d2985fd595b5c3
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2025-08-24 08:03:34 +0200

    Add ProductTree integration test

--- tests/integration/sap-api_test.ts
+++ tests/integration/sap-api_test.ts
@@ -1,10 +1,13 @@
 import { assertEquals, assertExists } from "@std/assert";
 import { ItemService } from "../../src/sap/services/item-service.ts";
-import { testItems } from "../fixtures/test_data.ts";
+import { testItems, testProductTrees } from "../fixtures/test_data.ts";
 import {
   setupItemTestData,
+  setupProductTreeTestData,
   teardownItemTestData,
+  teardownProductTreeTestData,
 } from "../fixtures/sap-setup.ts";
+import { ProductTreeService } from "../../src/sap/services/product-tree-service.ts";
 
 Deno.test(async function getItemTest() {
   await setupItemTestData();
@@ -16,6 +19,22 @@ Deno.test(async function getItemTest() {
     assertExists(actual);
     assertEquals(actual, expected);
   } finally {
+    await teardownItemTestData();
+  }
+});
+
+Deno.test(async function getProductTreeTest() {
+  await setupItemTestData();
+  await setupProductTreeTestData();
+
+  try {
+    const expected = testProductTrees["test_0000001111_test"];
+    const actual = await ProductTreeService.getProductTree(expected.treeCode);
+
+    assertExists(actual);
+    assertEquals(actual, expected);
+  } finally {
+    await teardownProductTreeTestData();
     await teardownItemTestData();
   }
 });
